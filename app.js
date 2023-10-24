9c61fe0421b6d592f0f1645c4e8f4adf04f41973
Tushar <tusharkumarchopra.com>
2023-10-24 15:20:29 +0530

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+
+require('dotenv').config();
+
 
 const express = require('express');
 const app = express();
@@ -49,7 +49,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(mongoSanitize());
  
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
@@ -66,51 +66,6 @@ const store = MongoStore.create({
 store.on("error", function(e) {
   console.log("SESSION STORE ERROR", e);
 })
-
-const scriptSrcUrls = [
-  "https://stackpath.bootstrapcdn.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://api.mapbox.com/",
-  "https://kit.fontawesome.com/",
-  "https://cdnjs.cloudflare.com/",
-  "https://cdn.jsdelivr.net",
-];
-const styleSrcUrls = [
-  "https://kit-free.fontawesome.com/",
-  "https://stackpath.bootstrapcdn.com/",
-  "https://api.mapbox.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://fonts.googleapis.com/",
-  "https://use.fontawesome.com/",
-];
-const connectSrcUrls = [
-  "https://api.mapbox.com/",
-  "https://a.tiles.mapbox.com/",
-  "https://b.tiles.mapbox.com/",
-  "https://events.mapbox.com/",
-];
-const fontSrcUrls = [];
-app.use(
-  helmet.contentSecurityPolicy({
-      directives: {
-          defaultSrc: [],
-          connectSrc: ["'self'", ...connectSrcUrls],
-          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-          workerSrc: ["'self'", "blob:"],
-          objectSrc: [],
-          imgSrc: [
-              "'self'",
-              "blob:",
-              "data:",
-              "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-              "https://images.unsplash.com/",
-          ],
-          fontSrc: ["'self'", ...fontSrcUrls],
-      },
-  })
-);
-
 
 //setting up session
 const sessionConfig = {
@@ -136,7 +91,6 @@ app.use(flash());
 app.use(passport.initialize());  
 app.use(passport.session()); //session should be used before passport.session
 passport.use(new LocalStrategy(User.authenticate()));
-
 
 //static method to store and de-store a user in the session
 passport.serializeUser(User.serializeUser());
